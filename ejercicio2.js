8278ec81f36314fb810ef9cd0b18a7ae9deb9ff4
NicolasRamos <nicolasramos.borner@gmail.com>
2022-11-30 14:27:25 -0300

    2.5

---
+++ ejercicio2.js
@@ -0,0 +1,15 @@
+const nombre = "Nicolas Ramos";
+const edad = 26;
+const dev = true;
+const fechaNacimiento= new Date("october 11 1996");
+
+const libros = {
+    title:"The Martian Chronicles",
+    author: "Ray Bradbury",
+    fecha: 1950,
+    url:"https://cdn.milenio.com/uploads/media/2019/10/26/sus-textos-son-considerados-como.jpg"
+}
+
+const lista = [nombre, edad, dev, fechaNacimiento, libros]
+
+console.log(lista);
